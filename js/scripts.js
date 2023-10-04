60ca5cdf813fc27c2bc0e36cb7964049fdbbde10
angelramirez-rivera <ramia149@gmatc.matc.edu>
2023-10-04 13:00:50 -0500

    update 2
    Project update

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var user = "Angel";
+var salutation = "Hello, ";
+var greeting = salutation + user + '! Welcome to our shoe store';
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting; 
+
+var title = "Retro Shoes";
+var header = title;
+var headerEl = document.getElementById("header");
+
+headerEl.textContent = header;
+
+
